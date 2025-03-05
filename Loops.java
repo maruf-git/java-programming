c3f8ebfb77bfd51d32509fa85526fae3b8971e1f
orion <munna.csehstu@gmail.com>
2025-03-06 01:42:53 +0600

    loops

---
+++ Loops.java
@@ -0,0 +1,24 @@
+
+
+public class Loops{
+    public static void main(String[] args) {
+        // for loop
+        // while loop
+        // do while loop
+
+        System.out.println("for loop:");
+        for(int i=0; i<=100; i++){
+            System.out.print(i);
+            System.out.print(" ");
+        }
+        System.out.println();
+        System.out.println("while loop:");
+        int i=0;
+        while(i<=100)
+        {
+            System.out.print(i);
+            System.out.print(" ");
+            i++;
+        }
+    }
+}
