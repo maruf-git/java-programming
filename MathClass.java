718b08b83e448056fdb2d134042c17c89aa30565
orion <munna.csehstu@gmail.com>
2025-03-06 01:06:36 +0600

    Math Class

---
+++ MathClass.java
@@ -0,0 +1,11 @@
+public class MathClass {
+    public static void main(String[] args){
+        // max
+        // min
+        // random returns 0.0 to 1.0 any random number
+        System.out.println(Math.max(5,6));
+        System.out.println(Math.min(5,6));
+        System.out.println((int)(Math.random()*100));
+
+    }
+}
